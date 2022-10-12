867e498f9a2264c3151476c91296bcbf3bf4b82c
David M. Rogers <predictivestatmech@gmail.com>
2022-10-12 10:38:29 -0400

    Added Hash class.

--- include/syclhash/hash.hpp
+++ include/syclhash/hash.hpp
@@ -8,7 +8,7 @@
 namespace syclhash {
 
 template <typename T, sycl::access::mode Mode>
-struct DeviceHash;
+class DeviceHash;
 
 template <typename T>
 class Hash {
@@ -21,7 +21,7 @@ class Hash {
     const int size_expt;
 
     Hash(int size_expt, sycl::queue &queue)
-        : alloc(1 << size_expt, queue)
+        : alloc(size_expt, queue)
         , cell(1 << size_expt)
         , keys(1 << size_expt)
         , next(1 << size_expt)
@@ -41,127 +41,8 @@ class Hash {
         });
     }
 
-    template <sycl::access::mode Mode>
+    template <typename U, sycl::access::mode Mode>
     friend class DeviceHash;
-};
-
-/** Cursor pointing at a specific cell in a linked
- *  list.
- */
-template<class T, sycl::access::mode Mode>
-class Cursor {
-    DeviceHash<T,Mode> &dh;
-    Ptr index;
-
-  public:
-    const Ptr key;
-
-    using iterator_category = std::forward_iterator_tag;
-    using difference_type = size_t;
-
-    using value_type = T;
-    using reference = T &;
-    using pointer = T *;
-
-    Cursor(DeviceHash<T,Mode> &dh, Ptr key, Ptr index)
-            : dh(dh), index(index), key(key) {
-        seek();
-    }
-
-    //< Is this cursor over an empty cell?
-    bool is_empty() {
-        return dh.keys[index] == null_ptr;
-    }
-    //< Is this cursor a null-pointer?
-    bool is_null() {
-        return index == null_ptr;
-    }
-
-    /** Seek forward to the next valid index --
-     * where dh.keys[index] == key
-     * or index == null_ptr
-     */
-    void seek() {
-        for(; index != null_ptr && dh.keys[index] != key
-            ; index = dh.next[index] );
-    }
-    reference operator *() {
-        return dh.cell[index];
-    }
-    //pointer operator ->() {
-    //    return dh.cell[index];
-    //}
-    // pre-increment
-    Cursor& operator ++() {
-        index = dh.next[index];
-        seek();
-        return *this;
-    }
-    // post-increment
-    Cursor operator++(int) {
-        Cursor tmp = *this;
-        ++(*this);
-        return tmp;
-    }
-    friend bool operator== (const Cursor& a, const Cursor& b) {
-        return a.index == b.index;
-    };
-    friend bool operator!= (const Cursor& a, const Cursor& b) {
-        return a.index != b.index;
-    };
-
-    /** Append the given `next` index to the end of this bucket.
-     */
-    Cursor<T,Mode> append(Ptr next) {
-        // Special case: initial key is always a bucket member.
-        if(dh.mod(key) == next) return Cursor<T,Mode>(dh, key, next);
-
-        Ptr val = next;
-        //dh.next[next] = null_ptr;
-
-        while(val != null_ptr) {
-            Ptr cur = index; // Start at index.
-            // Seek to the end of the bucket.
-            // TODO (optimization): Collapse pointer chain here.
-            for(; dh.next[cur] != null_ptr
-                ; cur = dh.next[cur]);
-        
-            auto v = sycl::atomic_ref<
-                            Ptr, sycl::memory_order::relaxed,
-                            sycl::memory_scope::device,
-                            sycl::access::address_space::global_space>(
-                                    dh.next[cur]);
-            val = null_ptr;
-            if(v.compare_exchange_weak(val, next)) {
-                break;
-            }
-        }
-            
-        return Cursor<T,Mode>(dh, key, next);
-    }
-
-    /** Return true if index was deleted, false if it was not present.
-     *
-     * side-effect: Invalidates the cursor.
-     *
-     * Note: internally, we set the `key` to `null_ptr`
-     * for deleted keys.
-     * These are skipped over during bucket iteration.
-     */
-    bool del() {
-        if(index == null_ptr) return false;
-        auto v = sycl::atomic_ref<
-                            Ptr, sycl::memory_order::relaxed,
-                            sycl::memory_scope::device,
-                            sycl::access::address_space::global_space>(
-                                    dh.keys[index]);
-        Ptr val = key;
-        bool ret = v.compare_exchange_strong(val, null_ptr);
-
-        // Whatever the result, this cursor is now invalid.
-        index = null_ptr;
-        return ret;
-    }
 };
 
 /** Iterate through a linked list of cells.
@@ -182,8 +63,87 @@ class Bucket {
 
     Bucket(DeviceHash<T,Mode> &dh, Ptr key)
             : dh(dh), key(key) {}
-    Cursor<T,Mode> begin() { return Cursor<T,Mode>(dh, key, dh.mod(key)); }
-    Cursor<T,Mode> end()   { return Cursor<T,Mode>(dh, key, null_ptr); }
+
+    /** iterator = a cursor pointing at a specific cell in a linked list.
+     */
+    class iterator {
+        friend class Bucket<T,Mode>;
+        DeviceHash<T,Mode> &dh;
+        Ptr index;
+    
+        /** Only friends can construct iterators.
+         */
+        iterator(DeviceHash<T,Mode> &dh, Ptr key, Ptr index)
+                : dh(dh), index(index), key(key) {
+            seek();
+        }
+
+        /** Seek forward to the next valid index --
+         * where dh.keys[index] == key
+         * or index == null_ptr
+         */
+        void seek() {
+            for(; index != null_ptr && dh.keys[index] != key
+                ; index = dh.next[index] );
+        }
+
+      public:
+        const Ptr key;
+
+        using iterator_category = std::forward_iterator_tag;
+        using difference_type = size_t;
+
+        using value_type = T;
+        using reference = T &;
+        using pointer = T *;
+
+        //< Is this cursor over an empty cell?
+        // (due to potential parallel acccess,
+        //  this function would not be stable)
+        //bool is_empty() {
+        //    return dh.keys[index] == null_ptr;
+        //}
+        //< Is this cursor a null-pointer?
+        bool is_null() const {
+            return index == null_ptr;
+        }
+
+        reference operator *() const {
+            return dh.cell[index];
+        }
+
+        // pre-increment
+        iterator &operator++() {
+            if(index != null_ptr) {
+                index = dh.next[index];
+                seek();
+            }
+            return *this;
+        }
+        // post-increment
+        iterator operator++(int) {
+            iterator tmp = *this;
+            ++(*this);
+            return tmp;
+        }
+        bool operator== (const iterator& b) const {
+            return index == b.index;
+        };
+        bool operator!= (const iterator& b) const {
+            return index != b.index;
+        };
+    };
+
+    iterator begin() {
+        /* This special case is correct, but not needed.
+         * Because unoccupied cells must have next set to null_ptr,
+         * seek() will find next[key] == null_ptr.
+        if(!dh.alloc.occupied(key)) {
+            return end();
+        }*/
+        return iterator(dh, key, dh.mod(key));
+    }
+    iterator end()   { return iterator(dh, key, null_ptr); }
 
     /** Put a new value into this bucket.
      *
@@ -191,13 +151,75 @@ class Bucket {
      *  Note - cursor will be == end(), a null_ptr, on error.
      */
     template <typename Group>
-    Cursor<T,Mode> put(Group g) {
-        // 1. allocate a free hash-index
-        Ptr next = 0; //dh.alloc(g, key);
-        // 2. link the next value onto the bucket
-        return begin().append(next);
+    iterator insert(Group g, const T& value) {
+        Ptr index = dh.mod(key);
+        // special case -- first insertion
+        if(dh.keys[index] == null_ptr && dh.alloc.try_alloc(g, index)) {
+            while(!dh.set_key(g, index, key, value)) {}
+            return begin();
+        }
+        // below: canonical index is already allocated somewhere.
+
+        while(1) { // Loop here because link-cell-by-CAS may not succeed.
+            index = dh.mod(key);
+
+            // seek to end of linked-list
+            for(; dh.keys[index] != null_ptr && dh.next[index] != null_ptr
+                ; index = dh.next[index]) { }
+
+            if(dh.keys[index] == null_ptr) {
+                // empty slot: re-use it
+                if(dh.set_key(g, index, key, value)) {
+                    return iterator(dh, key, index);
+                }
+            } else if(dh.next[index] == null_ptr) {
+                // reached end: allocate a new slot
+                Ptr loc = dh.alloc.alloc(g, key);
+                if(loc == null_ptr) { // memory is full
+                    return end();
+                }
+                dh.keys[loc] = key;
+                dh.cell[loc] = value;
+                // now that we have it, we must put loc at the end
+                while( !dh.set_next(g, index, loc)) {
+                    index = dh.mod(key);
+                    for(; dh.next[index] != null_ptr
+                        ; index = dh.next[index]) { }
+                }
+                return iterator(dh, key, loc);
+            }
+        };
     }
 
+    /** Return true if index was deleted, false if it was not present.
+     *
+     * Caution: Regardless of the output of this call,
+     *          your cursor is now invalid.
+     *
+     * Note: internally, we set the `key` to `null_ptr`
+     * for deleted keys.
+     * These are skipped over during bucket iteration.
+     */
+    bool erase(iterator position) {
+        if(position.index == null_ptr) return false;
+        auto v = sycl::atomic_ref<
+                            Ptr, sycl::memory_order::release,
+                            sycl::memory_scope::device,
+                            sycl::access::address_space::global_space>(
+                                    dh.keys[position.index]);
+        Ptr val = key;
+        return v.compare_exchange_strong(val, null_ptr);
+    }
+
+    //> Have the group leader call erase() and report back to the group.
+    template <typename Group>
+    bool erase(Group g, iterator position) {
+        bool result = false;
+        if(g.get_local_linear_id() == 0) {
+            result = del(position);
+        }
+        return sycl::select_from_group(g, result, 0);
+    }
 };
 
 /** Device side data structure for hash table.
@@ -207,12 +229,14 @@ class Bucket {
  * Requires size_expt < 32
  */
 template <typename T, sycl::access::mode Mode>
-struct DeviceHash {
+class DeviceHash {
+    friend class Bucket<T,Mode>;
+
     DeviceAlloc<Mode>                alloc;
     sycl::accessor<T, 1, Mode>        cell;
     sycl::accessor<Ptr, 1, Mode>      keys;  // key for each cell
     sycl::accessor<Ptr, 1, Mode>      next; // `next` pointer for each cell
-  //private:
+  public:
     const int size_expt;
 
     DeviceHash(Hash<T> &h, sycl::handler &cgh)
@@ -226,9 +250,81 @@ struct DeviceHash {
 
     /** Wrap the index into the range of valid keys, [0, 2**size_expt).
      */
-    Ptr mod(Ptr index) {
+    Ptr mod(Ptr index) const {
         return index & ((1<<(size_expt+1)) - 1);
     }
+
+    // bucket = (key % N) is the first index.
+    Bucket<T,Mode> operator[](Ptr key) {
+        return Bucket<T,Mode>(*this, key);
+    }
+
+    /** Convenience function to insert a key,value pair.
+     */
+    template <typename Group>
+    typename Bucket<T,Mode>::iterator
+    insert(Group g, Ptr key, const T&value) {
+        Bucket<T,Mode> bucket = (*this)[key];
+        return bucket.insert(g, value);
+    }
+
+    /** Where key was null_ptr, set to key
+     *  and fill its value atomically.
+     */
+    bool set_key(Ptr index, Ptr key, const T &value) {
+        auto v = sycl::atomic_ref<
+                            Ptr, sycl::memory_order::relaxed,
+                            sycl::memory_scope::device,
+                            sycl::access::address_space::global_space>(
+                                    keys[index]);
+        Ptr val = null_ptr;
+        if(!v.compare_exchange_weak(val, null_ptr-1,
+                            sycl::memory_order::acquire)) {
+            return false;
+        }
+        cell[index] = value;
+        val = null_ptr-1;
+        while(!v.compare_exchange_strong(val, key,
+                            sycl::memory_order::release)) {
+            // this is an error
+        };
+        return true;
+    }
+
+    //> Have the group leader call set_key and report the result.
+    template <typename Group>
+    bool set_key(Group g, Ptr index, Ptr key, const T &value) {
+        bool result = false;
+        if(g.get_local_linear_id() == 0) {
+            result = set_key(index, key, value);
+        }
+        return sycl::select_from_group(g, result, 0);
+    }
+
+    //> Link index -> loc by setting next[index]
+    bool set_next(Ptr index, Ptr loc) {
+        auto v = sycl::atomic_ref<
+                            Ptr, sycl::memory_order::relaxed,
+                            sycl::memory_scope::device,
+                            sycl::access::address_space::global_space>(
+                                    next[index]);
+        Ptr val = null_ptr;
+        return(v.compare_exchange_weak(val, loc,
+                            sycl::memory_order::release));
+    }
+
+    //> Have the group leader call set_next and report the result.
+    template <typename Group>
+    bool set_next(Group g, Ptr index, Ptr loc) {
+        bool result = false;
+        if(g.get_local_linear_id() == 0) {
+            result = set_next(index, loc);
+        }
+        return sycl::select_from_group(g, result, 0);
+    }
+};
+
+}
 
     /** Increment index in a pseudo-random way
      * that causes different groups to diverge in their
@@ -295,18 +391,3 @@ struct DeviceHash {
     }
     */
 
-    // bucket = (key % N) is the first index.
-    Bucket<T,Mode> get(Ptr key) {
-        return Bucket<T,Mode>(*this, key);
-    }
-
-    /** Convenience function to add one instance of `key`.
-     */
-    template <typename Group>
-    Cursor<T,Mode> put(Group g, Ptr key) {
-        Bucket<T,Mode> bucket = get(key);
-        return bucket.put(key);
-    }
-};
-
-}
--- tests/hash.cpp
+++ tests/hash.cpp
@@ -1,30 +1,61 @@
+#include <string>
+
 #include <syclhash/hash.hpp>
-
-struct Cell {
-    int data[10];
-};
-
 
 using namespace syclhash;
 
+template <typename T>
+void show(const sycl::stream &out, int id, T &x) {
+    std::stringstream ss;
+    ss << id << ":";
+    for(auto item : x) {
+        ss << " " << item;
+    }
+    ss << "\n";
+    std::string ans(ss.str());
+    out << ans.c_str();
+}
+
 int main() {
+    typedef int T;
     sycl::queue q;
     // 1024 cells
-    syclhash::Hash<Cell> hash(10, q);
+    syclhash::Hash<T> hash(10, q);
 
-    // Submit a kernel allocating lots of cells:
+    // Submit a kernel filling and emptying hashes by group
     q.submit([&](sycl::handler &cgh) {
         //sycl::accessor X{ans, cgh, sycl::write_only, sycl::no_init};
-        DeviceHash<Cell,sycl::access::mode::discard_write> dh(hash, cgh);
+        //DeviceHash<T,sycl::access::mode::discard_write> dh(hash, cgh);
+        DeviceHash<T,sycl::access::mode::read_write> dh1(hash, cgh);
+        sycl::stream out(1024, 256, cgh);
 
-        cgh.parallel_for(sycl::nd_range<1>(32,32), [=](sycl::nd_item<1> it) {
-            uint32_t gid = it.get_group_linear_id();
-            uint32_t tid = it.get_local_linear_id();
+        cgh.parallel_for(sycl::nd_range<1>(16,4), [=](sycl::nd_item<1> it) {
+            DeviceHash<T,sycl::access::mode::read_write> dh( dh1 );
+            int gid = it.get_group(0);
+            sycl::group<1> g = it.get_group();
 
-            //uint32_t loc = dh.get(it.get_group(), gid);
+            {
+                auto bucket = dh[gid];
+                bucket.insert(g, 1+10*gid);
+                bucket.insert(g, 2+10*gid);
+                bucket.insert(g, 3+10*gid);
+                if(it.get_local_id(0) == 0) {
+                    show(out, gid, bucket);
+                }
+            }
 
-            if(tid == 0) {
-            //    X[gid] = loc;
+            // get the next index over
+            if(1) {
+                auto bucket = dh[ (gid+1)%4 ];
+                //bucket.erase(++bucket.begin());
+                if(it.get_local_id(0) == 0) {
+                    show(out, gid, bucket);
+                }
+
+                //bucket.erase(bucket.begin());
+                //if(it.get_local_id(0) == 0) {
+                //    show(out, gid, bucket);
+                //}
             }
         });
     });
